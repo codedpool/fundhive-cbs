ad388f71fc6736fab03f778e635c930deb22d2e5
vrindabindal12 <vrindabindal1212@gmail.com>
2025-11-08 16:22:33 +0530

    nav

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { MewHeader } from './sections/MewHeader';
+import MewHeader from './sections/MewHeader';
 
 export function Login() { 
   const { loginWithRedirect } = useAuth0();
