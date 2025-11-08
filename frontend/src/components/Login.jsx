ed6fe24207f4400eb59dc03897a66c1fcea67d68
vrindabindal12 <vrindabindal1212@gmail.com>
2025-11-08 17:37:10 +0530

    plan

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,6 +3,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 import MewHeader from './sections/MewHeader';
 import { Hero } from './sections/Hero';
 import { ProductShowcase } from './sections/ProductShowcase';
+import { Pricing } from './sections/Pricing';
+
 
 export function Login() { 
   const { loginWithRedirect } = useAuth0();
@@ -13,6 +15,7 @@ export function Login() {
       <MewHeader />
       <Hero />
       <ProductShowcase />
+      <Pricing />
       </>
   );
 }
