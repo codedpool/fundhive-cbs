68645dec4e5dcf510e9f31670f48bd4dab142a7e
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-11-08 17:21:42 +0530

    updated ui

--- frontend/src/components/sections/MewHeader.jsx
+++ frontend/src/components/sections/MewHeader.jsx
@@ -8,24 +8,30 @@ const MewHeader = () => {
   const { loginWithRedirect } = useAuth0(); // ✅ optional
 
   return (
+    <>
     <header className="sticky top-0 backdrop-blur-sm z-20">
+       
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+       
         <p className="text-white/60 hidden md:block">
           Streamline your workflow and boost your productivity
         </p>
+        
         <div className="inline-flex gap-1 items-center">
           <p>Get started for free</p>
+          
           <img src={ArrowRight} alt="arrow" className="h-4 w-4 inline-flex" />
         </div>
       </div>
-
+      
       <div className="py-5 bg-transparent">
         <div className="container mx-auto px-6">
           <div className="flex items-center justify-between">
             <img src={Logo} alt="Saas Logo" height={40} width={40} />
             <img src={MenuIcon} alt="menu icon" className="h-5 w-5 md:hidden" />
+          <br />
 
-            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+            <nav className="hidden md:flex gap-4 text-black/60 items-center">
               <a href="#">About</a>
               <a href="#">Features</a>
               <a href="#">Customers</a>
@@ -44,6 +50,7 @@ const MewHeader = () => {
         </div>
       </div>
     </header>
+    </>
   );
 };
 
